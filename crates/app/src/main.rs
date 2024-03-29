4364072e59335b99561070eb1adac6bce4cd9cf1
ES.Skachkov <admin@biptik.com>
2024-03-30 03:06:21 +1000

    update cargo

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,9 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::path::*;
 use eframe::egui;
-use egui::*;
-use egui_file_dialog::{FileDialog};
+use egui_file_dialog::FileDialog;
 use pixelation;
+use std::path::*;
 #[derive(Default)]
 struct MyApp {
     file_dialog: FileDialog,
@@ -27,47 +26,50 @@ impl eframe::App for MyApp {
                 // Open the file dialog to select a file.
                 self.file_dialog.select_file();
             }
-            
-            ui.label(format!("Selected file: {:?}", self.selected_file
-            ));
-                      
+
+            ui.label(format!("Selected file: {:?}", self.selected_file));
+
             // Update the dialog and check if the user selected a file
             if let Some(path) = self.file_dialog.update(ctx).selected() {
-                    self.selected_file = Some(path.to_path_buf());
-               
-                    // window 1
-                    ui.horizontal(|ui| { 
-                    ui.add( 
-                        egui::Image::new(format!("{:?}",path))
-                        .max_width(400.0)
-                        .max_height(400.0));
-                    
+                self.selected_file = Some(path.to_path_buf());
 
-                    
+                // window 1
+                ui.horizontal(|ui| {
+                    ui.add(
+                        egui::Image::new(format!("{:?}", path))
+                            .max_width(400.0)
+                            .max_height(400.0),
+                    );
+
                     //pixelation
                     let image = image::open(path);
                     match image::open(path) {
                         Ok(image) => {
                             image.save(r"./1.png").unwrap();
-                            let vec_colors = pixelation::dominant_colors(image.clone()); 
-                            let sqauare = pixelation::generate_squares(10.0, image::open(path.clone()).unwrap());
+                            let vec_colors = pixelation::dominant_colors(image.clone());
+                            let sqauare = pixelation::generate_squares(
+                                10.0,
+                                image::open(path.clone()).unwrap(),
+                            );
                             let line = pixelation::line(image.clone(), 10);
-                            let img_out = pixelation::paint_coordinats(sqauare.clone(), vec_colors.clone(), image.clone(), line.clone());
-                                img_out.clone().save(r".\out_1.png").unwrap();
+                            let img_out = pixelation::paint_coordinats(
+                                sqauare.clone(),
+                                vec_colors.clone(),
+                                image.clone(),
+                                line.clone(),
+                            );
+                            img_out.clone().save(r".\out_1.png").unwrap();
                             // window 2
-                            ui.add( 
+                            ui.add(
                                 egui::Image::new(r".\out_1.png")
-                                .max_width(400.0)
-                                .max_height(400.0));
-                            
+                                    .max_width(400.0)
+                                    .max_height(400.0),
+                            );
                         }
-                        Err(err) => {
-                            
-                        }
+                        Err(err) => {}
                     }
-                });     
+                });
             }
-            
         });
     }
 }
